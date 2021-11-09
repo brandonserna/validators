5dcf7e76510a237fb2683086a9aff12b53124aba
Brandon Serna <bserna@usgs.gov>
2021-11-09 08:54:31 -0700

    💄 fix import order

--- validators/__init__.py
+++ validators/__init__.py
@@ -16,8 +16,8 @@ from .extremes import Max, Min
 from .hashes import md5, sha1, sha224, sha256, sha512
 from .i18n import fi_business_id, fi_ssn
 from .iban import iban
-from .ip_address import ipv4, ipv4_cidr, ipv6, ipv6_cidr
 from .identifiers import orcid
+from .ip_address import ipv4, ipv4_cidr, ipv6, ipv6_cidr
 from .length import length
 from .mac_address import mac_address
 from .slug import slug
